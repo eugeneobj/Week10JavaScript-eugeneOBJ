0c3b708849b3ac7ab072c17a9a6dbe89a3521f80
Eugene, Ong Bang Jun <ongbe363@newschool.edu>
2016-11-02 18:15:05 -0400

    JavaScript Submission
    JavaScript Submission, unsuccessful.

---
+++ myscript.js
@@ -0,0 +1,23 @@
+//Var for all 100 lockers
+var myArray = [];
+
+// Loop sets all 100 lockers to '1'
+for (var i = 0; i < 100; i++) {
+	console.log(i);
+	myArray[i] = 1;
+}
+
+//for loop for all students
+for (var i = 0; i < 100; i++) {
+	
+	//for loop for one student to toggle the lockers
+	for (var i = 0; i < 100; i++) {
+		
+		//Conditions for If & Else
+		if 		(myArray[i] == 1;
+				myArray[i] == 0;)
+			
+		else 	(myArray[i] == 0;
+				myArray[i] == 1;)
+	}
+}
